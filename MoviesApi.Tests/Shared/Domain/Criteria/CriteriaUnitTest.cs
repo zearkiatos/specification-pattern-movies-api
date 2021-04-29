9c858a68f63b11af60d987eb33f7ee999591705a
pcapriles <pedro.capriles@walmart.com>
2021-04-29 09:07:24 -0400

    Working in test Should_DeclarateCriteria_And_Return_Associate_Data

---
+++ MoviesApi.Tests/Shared/Domain/Criteria/CriteriaUnitTest.cs
@@ -0,0 +1,17 @@
+using System;
+using Xunit;
+using System.Collections.Generic;
+using MoviesApi.Shared.Domain;
+namespace MoviesApi.Tests.Shared.Domain.Criteria
+{
+    public class CriteriaUnitTest
+    {
+        [Fact]
+        public void Should_DeclarateCriteria_And_Return_Associate_Data()
+        {
+            List<string> filters = new List<string>();
+            
+            
+        }
+    }
+}
