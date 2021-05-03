317b4f977b10c0b2cbbf9667656a3706f10a1f5d
zearkiatos <caprilespe@outlook.com>
2021-05-02 22:03:47 -0400

    Ending Movies model and add tests

--- MoviesApi/Movies/Domain/Movie.cs
+++ MoviesApi/Movies/Domain/Movie.cs
@@ -1,9 +1,10 @@
+using Newtonsoft.Json;
 
 namespace MoviesApi.Movies.Domain
 {
-    class Movies
+    public class Movie
     {
-        public Movies(int id, string url, string title, int year, string summary, string[] genres, string description, string youtubeTrailerCode, string backgroundImage, string mediumCoverImage, int runtime, double rating)
+        public Movie(int id, string url, string title, int year, string summary, string[] genres, string description, string synopsis, string youtubeTrailerCode, string backgroundImage, string mediumCoverImage, int runtime, double rating)
         {
             this.id = id;
             this.url = url;
@@ -12,89 +13,92 @@ namespace MoviesApi.Movies.Domain
             this.summary = summary;
             this.genres = genres;
             this.description = description;
+            this.synopsis = synopsis;
             this.youtubeTrailerCode = youtubeTrailerCode;
             this.backgroundImage = backgroundImage;
             this.mediumCoverImage = mediumCoverImage;
             this.runtime = runtime;
             this.rating = rating;
         }
+        [JsonProperty("id")]
         private int id;
         public int Id
         {
             get { return id; }
         }
-
+        [JsonProperty("url")]
         private string url;
         public string Url
         {
             get { return url; }
         }
-
+        [JsonProperty("title_english")]
         private string title;
         public string Title
         {
             get { return title; }
         }
-
+        [JsonProperty("year")]
         private int year;
         public int Year
         {
             get { return year; }
         }
-
+        [JsonProperty("summary")]
         private string summary;
         public string Summary
         {
             get { return summary; }
         }
 
+        [JsonProperty("genres")]
         private string[] genres;
         public string[] Genres
         {
             get { return genres; }
         }
-
+        [JsonProperty("description_full")]
         private string description;
         public string Description
         {
             get { return description; }
         }
-
+        [JsonProperty("synopsis")]
+        private string synopsis;
+        public string Synopsis
+        {
+            get { return synopsis; }
+        }
+        
+        [JsonProperty("yt_trailer_code")]
         private string youtubeTrailerCode;
         public string YoutubeTrailerCode
         {
             get { return youtubeTrailerCode; }
         }
-
+        [JsonProperty("background_image")]
         private string backgroundImage;
         public string BackgroundImage
         {
             get { return backgroundImage; }
         }
-
+        [JsonProperty("medium_cover_image")]
         private string mediumCoverImage;
         public string MediumCoverImage
         {
             get { return mediumCoverImage; }
         }
-
+        [JsonProperty("runtime")]
         private int runtime;
         public int Runtime
         {
             get { return runtime; }
         }
-
+        [JsonProperty("rating")]
         private double rating;
         public double Rating
         {
             get { return rating; }
         }
-
-
-
-
-
-
-
     }
 }
