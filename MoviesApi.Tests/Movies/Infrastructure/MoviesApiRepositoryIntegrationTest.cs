6dbf95540d5f3a85a89d888e8c0586a623258b78
pcapriles <pedro.capriles@walmart.com>
2021-05-06 08:52:22 -0400

    Delete weather controller and model

---
+++ MoviesApi.Tests/Movies/Infrastructure/MoviesApiRepositoryIntegrationTest.cs
@@ -0,0 +1,12 @@
+using System;
+using Xunit;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+using MoviesApi.Movies.Domain;
+namespace MoviesApi.Tests.Movies.Infrastructure
+{
+    public class MoviesApiRepositoryIntegrationTest
+    {
+
+    }
+}
